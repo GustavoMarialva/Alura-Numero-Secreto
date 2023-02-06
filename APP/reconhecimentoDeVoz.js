2f212ef2cb850c428ccd2933ffb56f7a257743cd
Gustavo Marialva <gmarialva@gmail.com>
2023-02-06 13:55:05 -0300

    adiciona api web speech

--- APP/reconhecimentoDeVoz.js
+++ APP/reconhecimentoDeVoz.js
@@ -1,6 +1,26 @@
+const elementoChute = document.getElementById("chute");
+
 const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 
 recognition.lang = "pt-br";
 recognition.start();
+
+recognition.addEventListener("result", onSpeak);
+
+function onSpeak(e) {
+  chute = e.results[0][0].transcript;
+  exibeChuteNaTela(chute);
+}
+
+// Aprendemos a acessar o conteúdo falado pelo usuário através da API de reconhecimento de voz SpeechRecognition.
+
+// Para isso adicionamos um evento e acessamos a propriedade “results”, depois as duas arrays de índice “0” e por fim o “transcript” que é onde fica o conteúdo falado.
+
+function exibeChuteNaTela(chute) {
+  elementoChute.innerHTML = `
+      <div>Você disse</div>
+      <span class='box'>${chute}</span>
+    `;
+}
