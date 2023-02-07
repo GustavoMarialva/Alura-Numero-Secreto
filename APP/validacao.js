df98ba47e9e16ed56351e80558cc0ce4e620a028
Gustavo Marialva <gmarialva@gmail.com>
2023-02-07 14:02:06 -0300

    valida numero acertado

--- APP/validacao.js
+++ APP/validacao.js
@@ -3,13 +3,19 @@ function verificaSeOChutePossuiUmValorValido() {
   // vai validar se ele é um número inteiro
 
   if (chuteForInvalido(numero)) {
-    console.log("Valor inválido");
+    elementoChute.innerHTML += "<div>Valor inválido</div>";
   }
 
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-    console.log(
-      `Número inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}`
-    );
+    elementoChute.innerHTML += `<div>
+        Número inválido: Fale um número entre ${menorValor} e ${maiorValor}
+      </div>`;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+    <h2>Você acertou!!!!</h2>
+    <h3>O número secreto era ${numeroSecreto}</h3>`;
   }
 }
 function chuteForInvalido(numero) {
