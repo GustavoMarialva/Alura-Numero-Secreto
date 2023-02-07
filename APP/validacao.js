86a84d1d97e282a2671e3d7e1c2d74c350cc14fc
Gustavo Marialva <gmarialva@gmail.com>
2023-02-07 14:12:15 -0300

    adiciona dicas e finaliza o jogo

--- APP/validacao.js
+++ APP/validacao.js
@@ -4,18 +4,28 @@ function verificaSeOChutePossuiUmValorValido() {
 
   if (chuteForInvalido(numero)) {
     elementoChute.innerHTML += "<div>Valor inválido</div>";
+    return;
   }
 
   if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
     elementoChute.innerHTML += `<div>
         Número inválido: Fale um número entre ${menorValor} e ${maiorValor}
       </div>`;
+    return;
   }
 
   if (numero === numeroSecreto) {
     document.body.innerHTML = `
     <h2>Você acertou!!!!</h2>
     <h3>O número secreto era ${numeroSecreto}</h3>`;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>
+    O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i>
+  </div>`;
+  } else {
+    elementoChute.innerHTML += `<div>
+    O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i>
+  </div>`;
   }
 }
 function chuteForInvalido(numero) {
